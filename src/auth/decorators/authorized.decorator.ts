691e45ef4a914b1dd84dbc33f69b8e8655ac1ba3
iluha <specnaz31@yandex.ru>
2025-05-30 17:18:38 +0300

    del import

--- src/auth/decorators/authorized.decorator.ts
+++ src/auth/decorators/authorized.decorator.ts
@@ -1,6 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { Request } from 'express';
 interface AuthenticatedRequest {
   user?: User;
 }
